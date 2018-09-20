eafc54f9ed4555b6304181d8c64e896b93e1c4c4
muttsu-623 <mutsumi1996@gmail.com>
2018-09-20 12:17:08 +0900

    I tried to display particle trackes using TEveLine but I couldn't it.

--- sample.C
+++ sample.C
@@ -14,24 +14,22 @@
 #define __PARTICLE_H__
 
 #include "TObject.h"
-#include "TPolyLine3D.h"
 
 class Particle : public TEveLine
 {
   public :
-    Particle(const char *_name);
+    Particle();
     void SetMomentum();
 
-    double _px; 
-    double _py; 
-    double _pz; 
+    double _px;
+    double _py;
+    double _pz;
 
   ClassDef(Particle,1) // 自分で定義したクラスを利用する時に必要。
 };
 
 // constractor
-Particle::Particle(const char *_name) :  // TPolyLine3D()の引数は
-                        TEveLine(_name), 
+Particle::Particle() :  TEveLine(), 
                         _px(0),
                         _py(0), 
                         _pz(0)
@@ -58,12 +56,14 @@ class TGNumberEntryField;
 class TPolyLine3D;
 class TLegend;
 class Particle;
+class TEveManager;
 
 class Event : public TObject
 {
   public :
-    Event();
-    void SetCanvas(TCanvas* in);
+    Event(TEveManager *in);
+    // void SetCanvas(TCanvas* in);
+    // void SetEveManager(TEveManager* in);
     void OpenFile(); 
     void SetNField(TGNumberEntryField* in);
     void Update();
@@ -82,7 +82,8 @@ class Event : public TObject
     int _ev;
     int _ev_max;
 
-    TCanvas* _canvas;
+    // TCanvas* _canvas;
+    TEveManager* _manager;
     TNtupleD* _tup; // double型のみ扱えるTTree
 
     TGFileDialog* _fdialog; 
@@ -110,9 +111,10 @@ class Event : public TObject
 };
 
 // constractor
-Event::Event() : // メンバ変数の初期化．この場合はコンストラクタ内でも良いが、constの場合はここで記述する必要がある。()内は初期値。
+Event::Event(TEveManager *in) : // メンバ変数の初期化．この場合はコンストラクタ内でも良いが、constの場合はここで記述する必要がある。()内は初期値。
                  _ev(0),
                  _ev_max(0),
+                 _manager(in),
                  // ファイルに関するもの
                  _fileinfo(0),
                  _fdialog(0),
@@ -139,6 +141,7 @@ void Event::OpenFile()
   _ev = 0;
   _ev_field->SetNumber(_ev); // _ev_fieldに_evの値をセット。
 
+  /*
   if (_mc_b) delete _mc_b;
   if (_rc_b) delete _rc_b;
   if (_mc_bbar) delete _mc_bbar;
@@ -151,7 +154,6 @@ void Event::OpenFile()
   if (_yline) delete _yline;
   if (_zline) delete _zline;
   if (_file) delete _file;
-  /*
   if (_fileinfo) delete _fileinfo;
   if (_fdialog) delete _fdialog;
   */
@@ -161,111 +163,129 @@ void Event::OpenFile()
   _fdialog = new TGFileDialog(gClient->GetDefaultRoot(),0,kFDOpen,_fileinfo);
 
   _file = new TFile(_fileinfo->fFilename);
+
   _tup = static_cast<TNtupleD*>(_file->Get("hAnl")); 
   _ev_max = _tup->GetEntries(); // TTreeのEntryの数を _ev_maxに入れている
   std::cerr << "Total # of events = " << _ev_max << std::endl;
 
   // b quark
-  _mc_b = new Particle("mc_b");
-    _mc_b->SetLineColor(2);
+  _mc_b = new Particle;
+    _mc_b->SetMainColor(kRed);
     _mc_b->SetLineStyle(2);
-  _rc_b = new Particle("rc_b");
-    _rc_b->SetLineColor(2);
-    _rc_b->SetLineStyle(1);
+  // _rc_b = new Particle;
+    // _rc_b->SetLineColor(kRed);
+    // _rc_b->SetLineStyle(1);
   // bbar quark
-  _mc_bbar = new Particle("mc_bbar");
-    _mc_bbar->SetLineColor(4);
-    _mc_bbar->SetLineStyle(2);
-  _rc_bbar = new Particle("rc_bbar");
-    _rc_bbar->SetLineColor(4);
-    _rc_bbar->SetLineStyle(1);
+  // _mc_bbar = new Particle();
+    // _mc_bbar->SetLineColor(kGreen);
+    // _mc_bbar->SetLineStyle(2);
+  // _rc_bbar = new Particle();
+    // _rc_bbar->SetLineColor(kGreen);
+    // _rc_bbar->SetLineStyle(1);
   // W+ boson 
-  _mc_wp = new Particle("mc_wp");
-    _mc_wp->SetLineColor(6);
-    _mc_wp->SetLineStyle(2);
-  _rc_wp = new Particle("rc_wp");
-    _rc_wp->SetLineColor(6);
-    _rc_wp->SetLineStyle(1);
+  // _mc_wp = new Particle();
+    // _mc_wp->SetLineColor(kBlue);
+    // _mc_wp->SetLineStyle(2);
+  // _rc_wp = new Particle();
+    // _rc_wp->SetLineColor(kBlue);
+    // _rc_wp->SetLineStyle(1);
   // W- boson 
-  _mc_wm = new Particle("mc_wm");
-    _mc_wm->SetLineColor(7);
-    _mc_wm->SetLineStyle(2);
-  _rc_wm = new Particle("rc_wm");
-    _rc_wm->SetLineColor(7);
-    _rc_wm->SetLineStyle(1);
+  // _mc_wm = new Particle();
+    // _mc_wm->SetLineColor(kYellow);
+    // _mc_wm->SetLineStyle(2);
+  // _rc_wm = new Particle();
+    // _rc_wm->SetLineColor(kYellow);
+    // _rc_wm->SetLineStyle(1);
 
   // TTreeの値をそれぞれ代入している。
   _tup->SetBranchAddress("b_px", &_mc_b->_px); // TTreeから"b_px"を取り出して、_mc_bのpxに入れている。
   _tup->SetBranchAddress("b_py", &_mc_b->_py); // TTreeから"b_py"を取り出して、_mc_bのpyに入れている。
   _tup->SetBranchAddress("b_pz", &_mc_b->_pz); // TTreeから"b_pz"を取り出して、_mc_bのpzに入れている。
-  _tup->SetBranchAddress("b_px_rec", &_rc_b->_px);
-  _tup->SetBranchAddress("b_py_rec", &_rc_b->_py);
-  _tup->SetBranchAddress("b_pz_rec", &_rc_b->_pz);
-  _tup->SetBranchAddress("bbar_px", &_mc_bbar->_px);
-  _tup->SetBranchAddress("bbar_py", &_mc_bbar->_py);
-  _tup->SetBranchAddress("bbar_pz", &_mc_bbar->_pz);
-  _tup->SetBranchAddress("bbar_px_rec", &_rc_bbar->_px);
-  _tup->SetBranchAddress("bbar_py_rec", &_rc_bbar->_py);
-  _tup->SetBranchAddress("bbar_pz_rec", &_rc_bbar->_pz);
-  _tup->SetBranchAddress("wp_px", &_mc_wp->_px);
-  _tup->SetBranchAddress("wp_py", &_mc_wp->_py);
-  _tup->SetBranchAddress("wp_pz", &_mc_wp->_pz);
-  _tup->SetBranchAddress("wp_px_rec", &_rc_wp->_px);
-  _tup->SetBranchAddress("wp_py_rec", &_rc_wp->_py);
-  _tup->SetBranchAddress("wp_pz_rec", &_rc_wp->_pz);
-  _tup->SetBranchAddress("wm_px", &_mc_wm->_px);
-  _tup->SetBranchAddress("wm_py", &_mc_wm->_py);
-  _tup->SetBranchAddress("wm_pz", &_mc_wm->_pz);
-  _tup->SetBranchAddress("wm_px_rec", &_rc_wm->_px);
-  _tup->SetBranchAddress("wm_py_rec", &_rc_wm->_py);
-  _tup->SetBranchAddress("wm_pz_rec", &_rc_wm->_pz);
+  _tup->GetEntry(_ev);
+  // _tup->SetBranchAddress("b_px_rec", &_rc_b->_px);
+  // _tup->SetBranchAddress("b_py_rec", &_rc_b->_py);
+  // _tup->SetBranchAddress("b_pz_rec", &_rc_b->_pz);
+  // _tup->SetBranchAddress("bbar_px", &_mc_bbar->_px);
+  // _tup->SetBranchAddress("bbar_py", &_mc_bbar->_py);
+  // _tup->SetBranchAddress("bbar_pz", &_mc_bbar->_pz);
+  // _tup->SetBranchAddress("bbar_px_rec", &_rc_bbar->_px);
+  // _tup->SetBranchAddress("bbar_py_rec", &_rc_bbar->_py);
+  // _tup->SetBranchAddress("bbar_pz_rec", &_rc_bbar->_pz);
+  // _tup->SetBranchAddress("wp_px", &_mc_wp->_px);
+  // _tup->SetBranchAddress("wp_py", &_mc_wp->_py);
+  // _tup->SetBranchAddress("wp_pz", &_mc_wp->_pz);
+  // _tup->SetBranchAddress("wp_px_rec", &_rc_wp->_px);
+  // _tup->SetBranchAddress("wp_py_rec", &_rc_wp->_py);
+  // _tup->SetBranchAddress("wp_pz_rec", &_rc_wp->_pz);
+  // _tup->SetBranchAddress("wm_px", &_mc_wm->_px);
+  // _tup->SetBranchAddress("wm_py", &_mc_wm->_py);
+  // _tup->SetBranchAddress("wm_pz", &_mc_wm->_pz);
+  // _tup->SetBranchAddress("wm_px_rec", &_rc_wm->_px);
+  // _tup->SetBranchAddress("wm_py_rec", &_rc_wm->_py);
+  // _tup->SetBranchAddress("wm_pz_rec", &_rc_wm->_pz);
 
-  _beampipe = new TPolyLine3D(2);
-  _beampipe->SetLineStyle(7);
-  _beampipe->SetLineColor(5);
-  _beampipe->SetPoint(1, x_min, 0, 0); // (何本目の線か, 終点のx座標, 終点のy座標, 終点のz座標)
-  _beampipe->SetPoint(2, x_max, 0, 0);
-  _beampipe->SetLineWidth(10);
-  _beampipe->Draw();
+  _mc_b->SetMomentum();
+  // _rc_b->SetMomentum();
+  // _mc_bbar->SetMomentum();
+  // _rc_bbar->SetMomentum();
+  // _mc_wp->SetMomentum();
+  // _rc_wp->SetMomentum();
+  // _mc_wm->SetMomentum();
+  // _rc_wm->SetMomentum();
 
-  _yline = new TPolyLine3D(2);
-  _yline->SetLineStyle(7);
-  _yline->SetLineColor(2);
-  _yline->SetPoint(1, 0, y_min, 0);
-  _yline->SetPoint(2, 0, y_max, 0);
-  _yline->SetLineWidth(10);
-  _yline->Draw();
+  _manager->AddElement((TEveLine*) _mc_b);
+  _manager->Redraw3D(kTRUE);
 
-  _zline = new TPolyLine3D(2);
-  _zline->SetLineStyle(7);
-  _zline->SetLineColor(4);
-  _zline->SetPoint(1, 0, 0, z_min);
-  _zline->SetPoint(2, 0, 0, z_max);
-  _zline->SetLineWidth(10);
-  _zline->Draw();
+  // _beampipe = new TPolyLine3D(2);
+  // _beampipe->SetLineStyle(7);
+  // _beampipe->SetLineColor(5);
+  // _beampipe->SetPoint(1, x_min, 0, 0); // (何本目の線か, 終点のx座標, 終点のy座標, 終点のz座標)
+  // _beampipe->SetPoint(2, x_max, 0, 0);
+  // _beampipe->SetLineWidth(10);
+  // _beampipe->Draw();
 
-  _leg = new TLegend(0.1,0.7,0.25,0.9);
-  _leg->AddEntry(_beampipe,"beam axis","l");
-  _leg->AddEntry(_yline, "yline", "l");
-  _leg->AddEntry(_zline, "zline", "l");
-  _leg->AddEntry(_mc_b,"b (MC)","l");
-  _leg->AddEntry(_rc_b,"b (REC)","l");
-  _leg->AddEntry(_mc_bbar,"bbar (MC)","l");
-  _leg->AddEntry(_rc_bbar,"bbar (REC)","l");
-  _leg->AddEntry(_mc_wp,"W+ (MC)","l");
-  _leg->AddEntry(_rc_wp,"W+ (REC)","l");
-  _leg->AddEntry(_mc_wm,"W- (MC)","l");
-  _leg->AddEntry(_rc_wm,"W- (REC)","l");
+  // _yline = new TPolyLine3D(2);
+  // _yline->SetLineStyle(7);
+  // _yline->SetLineColor(2);
+  // _yline->SetPoint(1, 0, y_min, 0);
+  // _yline->SetPoint(2, 0, y_max, 0);
+  // _yline->SetLineWidth(10);
+  // _yline->Draw();
 
-  this->Update();
+  // _zline = new TPolyLine3D(2);
+  // _zline->SetLineStyle(7);
+  // _zline->SetLineColor(4);
+  // _zline->SetPoint(1, 0, 0, z_min);
+  // _zline->SetPoint(2, 0, 0, z_max);
+  // _zline->SetLineWidth(10);
+  // _zline->Draw();
+
+  // _leg = new TLegend(0.1,0.7,0.25,0.9);
+  // _leg->AddEntry(_beampipe,"beam axis","l");
+  // _leg->AddEntry(_yline, "yline", "l");
+  // _leg->AddEntry(_zline, "zline", "l");
+  // _leg->AddEntry(_mc_b,"b (MC)","l");
+  // _leg->AddEntry(_rc_b,"b (REC)","l");
+  // _leg->AddEntry(_mc_bbar,"bbar (MC)","l");
+  // _leg->AddEntry(_rc_bbar,"bbar (REC)","l");
+  // _leg->AddEntry(_mc_wp,"W+ (MC)","l");
+  // _leg->AddEntry(_rc_wp,"W+ (REC)","l");
+  // _leg->AddEntry(_mc_wm,"W- (MC)","l");
+  // _leg->AddEntry(_rc_wm,"W- (REC)","l");
+
+  // this->Update();
 }
 
-void Event::SetCanvas(TCanvas* in) 
-{
-  _canvas = in;     // embcのアドレスが入っている。
-  _canvas->cd();    // Set current canvas & pad.
-//   _world->Draw();   // ヒストグラムを描く.
-}
+// void Event::SetCanvas(TCanvas* in) 
+// {
+//   _canvas = in;     // embcのアドレスが入っている。
+//   _canvas->cd();    // Set current canvas & pad.
+// //   _world->Draw();   // ヒストグラムを描く.
+// }
+
+// void SetEveManager(TEveManager* in) {
+//   _manager = in;
+// }
 
 void Event::Next() 
 {
@@ -296,6 +316,8 @@ void Event::Update()
 
   if (_ev>=0 && _ev<_ev_max) {
     _tup->GetEntry(_ev); // TTreeのエントリーをevの数に指定。
+    std::cerr << "Get Entry is called" << std::endl;
+    std::cerr << "_mc_b's _px" << _mc_b->_px << std::endl;
   } else if (_ev<0){
     std::cerr << "### ERROR ### Event id must be more than 0." << std::endl;
     _ev=0;
@@ -307,79 +329,114 @@ void Event::Update()
   std::cerr << "_ev = " << _ev << std::endl;
   _ev_field->SetNumber(_ev);
  
-  _canvas->cd();                  // canvasを分割している場合、どの区画のcanvasに入るかのために重要。
-  _mc_b->SetMomentum();    // optionを代入し、Drawしている。
-  _rc_b->SetMomentum();
-  _mc_bbar->SetMomentum();
-  _rc_bbar->SetMomentum();
-  _mc_wp->SetMomentum();
-  _rc_wp->SetMomentum();
-  _mc_wm->SetMomentum();
-  _rc_wm->SetMomentum();
-  _leg->Draw();                   // 箱のDraw
-  _canvas->Update();
+  // _canvas->cd();                  // canvasを分割している場合、どの区画のcanvasに入るかのために重要。
+  // _mc_b->SetMomentum();    // optionを代入し、Drawしている。
+  // _rc_b->SetMomentum();
+  // _mc_bbar->SetMomentum();
+  // _rc_bbar->SetMomentum();
+  // _mc_wp->SetMomentum();
+  // _rc_wp->SetMomentum();
+  // _mc_wm->SetMomentum();
+  // _rc_wm->SetMomentum();
+  // _leg->Draw();                   // 箱のDraw
+  // _canvas->Update();
+  // _manager->AddElement((TEveLine*) _mc_b);
+  // _manager->Redraw3D(kTRUE);
+  _manager->DoRedraw3D();
 }
 
 #endif
 
 // main
-void sample() 
-{    
-    const float MAINFRAME_WIDTH  = 600.;
-    const float MAINFRAME_HEIGHT = 600.;
-    const float CANVAS_WIDTH     = 600.;
-    const float CANVAS_HEIGHT    = 560.;
+void sample() {
+  const float MAINFRAME_WIDTH  = 600.;
+  const float MAINFRAME_HEIGHT = 600.;
+  const float CANVAS_WIDTH     = 600.;
+  const float CANVAS_HEIGHT    = 560.;
 
-    TEveManager::Create(kFALSE); // this create a global pointer, gEve. kFALSE: not map widow yet.
+  TEveManager::Create(kFALSE); // this create a global pointer, gEve. kFALSE: not map widow yet.
+  
+  TGMainFrame* frm = new TGMainFrame(gClient->GetRoot());
     
-    TGMainFrame* frm = new TGMainFrame(gClient->GetRoot());
-      
-    // create eve canvas
-    TEvePad* pad = new TEvePad();
-    pad->SetFillColor(kGray);
+  // create eve canvas
+  TEvePad* pad = new TEvePad();
+  pad->SetFillColor(kWhite);
 
-    // create an embed GL viewer 
-    TGLEmbeddedViewer* embviewer = new TGLEmbeddedViewer(frm, pad);
-    frm->AddFrame(embviewer->GetFrame(), new TGLayoutHints(kLHintsNormal | kLHintsExpandX | kLHintsExpandY));
+  // create an embed GL viewer 
+  TGLEmbeddedViewer* embviewer = new TGLEmbeddedViewer(frm, pad);
+  frm->AddFrame(embviewer->GetFrame(), new TGLayoutHints(kLHintsNormal | kLHintsExpandX | kLHintsExpandY));
 
-    // create a GL viewer 
-    TEveViewer* viewer = new TEveViewer("GLViewer");
-    viewer->SetGLViewer(embviewer, embviewer->GetFrame());
-    viewer->AddScene(gEve->GetEventScene());
-    gEve->GetViewers()->AddElement(viewer);
+  // create a GL viewer 
+  TEveViewer* viewer = new TEveViewer("GLViewer");
+  viewer->SetGLViewer(embviewer, embviewer->GetFrame());
+  viewer->AddScene(gEve->GetEventScene());
+  gEve->GetViewers()->AddElement(viewer);
 
-    // display elements settings
+  // display elements settings
 
-        // // define a tube (cylinder)
-        // TEveGeoShape* mytube = new TEveGeoShape;
-        // mytube->SetShape(new TGeoTube(10, 10.1, 10)); // rmin, rmax, dz
-        // mytube->SetNSegments(100); // number of vertices
-        // mytube->RefMainTrans().SetPos(-10, -10, 10); // set position
-        // gEve->AddElement(mytube);
+      // // define a tube (cylinder)
+      // TEveGeoShape* mytube = new TEveGeoShape;
+      // mytube->SetShape(new TGeoTube(10, 10.1, 10)); // rmin, rmax, dz
+      // mytube->SetNSegments(100); // number of vertices
+      // mytube->RefMainTrans().SetPos(-10, -10, 10); // set position
+      // gEve->AddElement(mytube);
 
-        TEveLine* myline = new TEveLine;
-        myline->SetMainColor(kGreen);
-        myline->SetLineStyle(1);
-        myline->SetLineWidth(5);
-        myline->SetNextPoint(0,0,0);
-        myline->SetNextPoint(-10,-10, 0);
-        gEve->AddElement(myline);
+      // TEveLine* myline = new TEveLine;
+      // myline->SetMainColor(kGreen);
+      // myline->SetLineStyle(1);
+      // myline->SetLineWidth(5);
+      // myline->SetNextPoint(0,0,0);
+      // myline->SetNextPoint(-10,-10, 0);
+      // gEve->AddElement(myline);
 
-    // rendering.
-    gEve->Redraw3D(kTRUE);
+  // rendering.
+  gEve->Redraw3D(kTRUE);
 
-    // Frame1の作成。
-    TGHorizontalFrame* bframe1 = new TGHorizontalFrame(frm, CANVAS_WIDTH, CANVAS_HEIGHT, kFixedWidth);
-    frm->AddFrame(bframe1);
+  // イベントインスタンスの生成。
+  Event *ev = new Event(gEve);
 
-    // exitボタン。
-    TGTextButton* b_exit = new TGTextButton(bframe1,"Exit","gApplication->Terminate(0)");
-    bframe1->AddFrame(b_exit,new TGLayoutHints(kLHintsLeft));
+  // ev->SetCanvas(gEve->AddCanvasTab("canvas"));
+  // ev->SetEveManager(gEve);
 
-    // display preparation done.
-    frm->MapSubwindows();
-    frm->Layout();
-    frm->Resize(MAINFRAME_WIDTH,MAINFRAME_HEIGHT);
-    frm->MapWindow();
+  // Frame1の作成。
+  TGHorizontalFrame* bframe1 = new TGHorizontalFrame(frm,CANVAS_WIDTH,(MAINFRAME_HEIGHT-CANVAS_HEIGHT)/2.,kFixedWidth);
+  frm->AddFrame(bframe1);
+
+  // exitボタン。
+  TGTextButton* b_exit = new TGTextButton(bframe1,"Exit","gApplication->Terminate(0)");
+  bframe1->AddFrame(b_exit,new TGLayoutHints(kLHintsLeft));
+
+  // Open Fileボタン。
+  TGTextButton* b_open = new TGTextButton(bframe1,"Open File");
+  b_open->Connect("Clicked()", "Event", ev, "OpenFile()");
+  bframe1->AddFrame(b_open,new TGLayoutHints(kLHintsLeft));
+
+  // Frame2の作成。
+  TGHorizontalFrame* bframe2 = new TGHorizontalFrame(frm,CANVAS_WIDTH,(MAINFRAME_HEIGHT-CANVAS_HEIGHT)/2.,kFixedWidth);
+  frm->AddFrame(bframe2);
+
+  // Prevボタン。
+  TGTextButton* b_prev = new TGTextButton(bframe2,"Prev");
+  b_prev->Connect("Clicked()","Event",ev,"Prev()");
+  bframe2->AddFrame(b_prev,new TGLayoutHints(kLHintsCenterX));
+
+  // 何番目のイベントかを表示するGUIパーツ。
+  TGNumberEntry* ne = new TGNumberEntry(bframe2);
+  ne->Connect("ValueSet(Long_t)","Event",ev,"Goto()");
+  bframe2->AddFrame(ne,new TGLayoutHints(kLHintsCenterX));
+  ev->SetNField(ne->GetNumberEntry());
+
+  // Nextボタン
+  TGTextButton* b_next = new TGTextButton(bframe2,"Next");
+  b_next->Connect("Clicked()","Event",ev,"Next()");
+  bframe2->AddFrame(b_next,new TGLayoutHints(kLHintsCenterX));
+
+  // display preparation done.
+  frm->MapSubwindows();
+  frm->Layout();
+  frm->Resize(MAINFRAME_WIDTH,MAINFRAME_HEIGHT);
+  frm->MapWindow();
+
+  ev->OpenFile();
 }
 
