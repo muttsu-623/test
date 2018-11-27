ca71f1f2ad8415b7709396dd4f47e9233f345997
muttsu-623 <mutsumi1996@gmail.com>
2018-11-27 17:55:18 +0900

    Refactor as I can read code easily

--- Event.cc
+++ Event.cc
@@ -21,55 +21,54 @@ Event::Event() : // メンバ変数の初期化．この場合はコンストラ
                  _lcReader(0) {}
 
 void Event::OpenFile() {
+    using std::cout;
+    using std::endl;
+    using std::string;
+    using std::vector;
+
     _ev = 0;
 
     _fileinfo = new TGFileInfo();
     _fileinfo->fIniDir=(char*)".";
     _fdialog = new TGFileDialog(gClient->GetDefaultRoot(),0,kFDOpen,_fileinfo);
-    std::cout << "Selected file name = " << _fileinfo->fFilename << std::endl;
+    cout << "Selected file name = " << _fileinfo->fFilename << endl;
 
     _lcReader = lcio::LCFactory::getInstance()->createLCReader(IO::LCReader::directAccess);
     _lcReader->open(_fileinfo->fFilename);
 
-    std::cout  << std::endl <<  "     "  << _fileinfo->fFilename
+    cout  << endl <<  "     "  << _fileinfo->fFilename
     <<  "     [ number of runs: "    <<  _lcReader->getNumberOfRuns() 
     <<       ", number of events: "  <<  _lcReader->getNumberOfEvents() << " ] "   
-    << std::endl
-    << std::endl; 
+    << endl
+    << endl; 
 
     EVENT::LCEvent* evt ;
     int nEvents = 0 ;
 
     //----------- the event loop -----------
-    while( (evt = _lcReader->readNextEvent()) != 0 ) {
+    while((evt = _lcReader->readNextEvent()) != 0) {
 
-        std::cout << std::endl 
-        << "============================================================================" << std::endl ;
-        std::cout << "        Event  : " << evt->getEventNumber() 
+        cout << endl 
+        << "============================================================================" << endl ;
+        cout << " Event  : " << evt->getEventNumber() 
         << " - run:  "         << evt->getRunNumber()
         << " - timestamp "     << evt->getTimeStamp()   
         << " - weight "        << evt->getWeight()   
-        << std::endl ;
-        std::cout << "============================================================================" << std::endl ;    
+        << endl ;
+        cout << "============================================================================" << endl ;    
 
-        UTIL::LCTime evtTime( evt->getTimeStamp() ) ;
-        std::cout << " date:      "      << evtTime.getDateString() << std::endl ;     
-        std::cout << " detector : "      << evt->getDetectorName() << std::endl ;
+        // UTIL::LCTime evtTime( evt->getTimeStamp() ) ;
+        // cout << " date:      "      << evtTime.getDateString() << endl ;     
+        cout << " detector : "      << evt->getDetectorName() << endl ;
 
-        const std::vector< std::string >* strVec = evt->getCollectionNames() ;
+        const vector< string >* strVec = evt->getCollectionNames() ;
 
         // loop over all collections:
-        std::vector< std::string >::const_iterator name ;
+        vector< string >::const_iterator name ;
 
-        for( name = strVec->begin() ; name != strVec->end() ; name++){
+        for(name = strVec->begin() ; name != strVec->end() ; name++){
             EVENT::LCCollection* col = evt->getCollection( *name ) ;
-
-            // call the detailed print functions depending on type name
-            if( evt->getCollection( *name )->getTypeName() == "MCParticle" ){
-
-                printMCParticles( col ) ;
-
-            }
+            if(evt->getCollection( *name )->getTypeName() == "MCParticle") printMCParticles(col) ;
         }
 
         nEvents ++ ;
@@ -78,40 +77,45 @@ void Event::OpenFile() {
 }
 
 void Event::printMCParticles(const EVENT::LCCollection* col ) {
+    using std::cout;
+    using std::endl;
+    using std::string;
+    using std::vector;
+    using std::map;
 
-    if( col->getTypeName() != EVENT::LCIO::MCPARTICLE ){
+    if(col->getTypeName() != EVENT::LCIO::MCPARTICLE){
 
-        std::cout << " collection not of type " << EVENT::LCIO::MCPARTICLE << std::endl ;
+        cout << " collection not of type " << EVENT::LCIO::MCPARTICLE << endl ;
         return ;
     }
 
-    std::cout << std::endl 
+    cout << endl 
     << "--------------- " << "print out of "  << EVENT::LCIO::MCPARTICLE << " collection "
-    << "--------------- " << std::endl ;
+    << "--------------- " << endl ;
 
     int nParticles =  col->getNumberOfElements() ;
 
     // fill map with particle pointers and collection indices
-    typedef std::map< EVENT::MCParticle*, int > PointerToIndexMap ;
+    typedef map< EVENT::MCParticle*, int > PointerToIndexMap ;
     PointerToIndexMap p2i_map ;
-    std::vector<EVENT::MCParticle*> moms ;
+    vector<EVENT::MCParticle*> moms ;
 
-    for( int k=0; k<nParticles; k++){
-        EVENT::MCParticle* part =  dynamic_cast<EVENT::MCParticle*>( col->getElementAt( k ) ) ;
+    for(int k=0; k<nParticles; k++){
+        EVENT::MCParticle* part =  dynamic_cast<EVENT::MCParticle*>(col->getElementAt(k)) ;
         p2i_map[ part ] = k ; 
-
         moms.push_back( part ) ;
     }
 
-    std::cout << std::endl
-    <<  "[   id   ]index|      PDG |    px,     py,        pz    | px_ep,   py_ep , pz_ep      | energy  |gen|[simstat ]| vertex x,     y   ,   z     | endpoint x,    y  ,   z     |    mass |  charge |            spin             | colorflow | [parents] - [daughters]"    
-    << std::endl 
-    << std::endl ;
+    cout << endl
+    <<  "[   id   ]index|       PDG|"
+    <<  "    px   ,    py   ,    pz   |  px_ep  ,  py_ep  ,  pz_ep  | energy  |gen| vertex x,    y    ,    z    | endpoint x,     y   ,    z    |    mass |  charge |            spin             | colorflow | [parents] - [daughters]"    
+    << endl 
+    << endl ;
 
     // loop over collection - preserve order
-    for(  int index = 0 ; index < nParticles ; index++){
+    for(int index = 0 ; index < nParticles ; index++){
 
-        EVENT::MCParticle* part =  dynamic_cast<EVENT::MCParticle*>( col->getElementAt( index ) ) ;
+        EVENT::MCParticle* part = dynamic_cast<EVENT::MCParticle*>(col->getElementAt(index)) ;
 
         printf("[%8.8d]", part->id() );
         printf("%5d|"   , index );
@@ -125,13 +129,12 @@ void Event::printMCParticles(const EVENT::LCCollection* col ) {
         part->getMomentumAtEndpoint()[1] , 
         part->getMomentumAtEndpoint()[2] );
         printf("% 1.2e|" , part->getEnergy() ) ; 
-
         printf(" %1d |" , part->getGeneratorStatus()  );
         printf("% 1.2e,% 1.2e,% 1.2e|" , 
         part->getVertex()[0] , 
         part->getVertex()[1] , 
         part->getVertex()[2] );
-        printf("% 1.2e,% 1.2e,% 1.2e|" , 
+        printf("  % 1.2e,% 1.2e,% 1.2e|" , 
         part->getEndpoint()[0] , 
         part->getEndpoint()[1] ,  
         part->getEndpoint()[2] );
@@ -147,21 +150,20 @@ void Event::printMCParticles(const EVENT::LCCollection* col ) {
         part->getColorFlow()[0] ,
         part->getColorFlow()[1] );
 
-        std::cout << " [" ;
+        cout << " [" ;
 
-        for(unsigned int k=0;k<part->getParents().size();k++){
-            if(k>0) std::cout << "," ;
-            std::cout << p2i_map[ part->getParents()[k] ]  ;
+        for(unsigned int k=0;k<part->getParents().size();k++) {
+            if(k>0) cout << "," ;
+            cout << p2i_map[ part->getParents()[k] ]  ;
         }
-        std::cout << "] - [" ;
-        for(unsigned int k=0;k<part->getDaughters().size();k++){
-            if(k>0) std::cout << "," ;
-            std::cout << p2i_map[ part->getDaughters()[k] ]  ;
+        cout << "] - [" ;
+        for(unsigned int k=0;k<part->getDaughters().size();k++) {
+            if(k>0) cout << "," ;
+            cout << p2i_map[ part->getDaughters()[k] ]  ;
         }
-        std::cout << "] " << std::endl ;
+        cout << "] " << endl ;
     }
-
-    std::cout << std::endl 
+    cout << endl 
     << "-------------------------------------------------------------------------------- " 
-    << std::endl ;
+    << endl ;
 }
